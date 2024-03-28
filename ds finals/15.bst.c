29ea4bb9a89b73f6e10c0db51fdaead1909fc7b6
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-03-28 11:46:41 +0530

    Update 15.bst.c

--- ds finals/15.bst.c
+++ ds finals/15.bst.c
@@ -5,25 +5,25 @@ int key;
 struct Node *left, *right;
 };
 struct Node* createNode(int key) {
-struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-newNode->key = key;
-newNode->left = newNode->right = NULL;
-return newNode;
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->key = key;
+        newNode->left = newNode->right = NULL;
+        return newNode;
 }
 struct Node* insert(struct Node* root, int key) {
-if (root == NULL)
-return createNode(key);
-if (key < root->key)
-root->left = insert(root->left, key);
-else if (key > root->key)
-root->right = insert(root->right, key);
-return root;
+        if (root == NULL)
+        return createNode(key);
+        if (key < root->key)
+        root->left = insert(root->left, key);
+        else if (key > root->key)
+        root->right = insert(root->right, key);
+        return root;
 }
 void inorderTraversal(struct Node* root) {
-if (root != NULL) {
-inorderTraversal(root->left);
-printf("%d ", root->key);
-inorderTraversal(root->right);
+        if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->key);
+        inorderTraversal(root->right);
 }
 }
 void preorderTraversal(struct Node* root) {
